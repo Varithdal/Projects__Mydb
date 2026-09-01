fdb2a5a8506f7349fc384fc25d741bab9ab39e1b
Varithdal <rbareo25@gmail.com>
2026-08-31 22:23:16 -0400

    Added basic initialization for the db tables on backend

--- src-tauri/src/db_api/mod.rs
+++ src-tauri/src/db_api/mod.rs
@@ -16,8 +16,122 @@ impl Database {
 
 	pub fn init(&self) -> Result<(), Errors> {
 		self.connection
-			.execute("", ())
-			.map_err(|_| Errors::SqlExecutionError)?;
+			.execute("PRAGMA foreign_keys = ON;", ())
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Profiles (
+						id INTEGER PRIMARY KEY,
+						name TEXT NOT NULL
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Media (
+						id INTEGER PRIMARY KEY,
+						profile_id INTEGER NOT NULL,
+						name TEXT NOT NULL,
+						filepath TEXT NOT NULL,
+						description TEXT,
+
+						UNIQUE (profile_id, id),
+
+						FOREIGN KEY (profile_id) REFERENCES Profiles(id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Videos (
+						id INTEGER PRIMARY KEY,
+						length INTEGER,
+						width INTEGER,
+						height INTEGER,
+
+						FOREIGN KEY (id) REFERENCES Media(id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Audio (
+						id INTEGER PRIMARY KEY,
+						length INTEGER NOT NULL,
+
+						FOREIGN KEY (id) REFERENCES Media(id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Images (
+						id INTEGER PRIMARY KEY,
+						width INTEGER,
+						height INTEGER,
+
+						FOREIGN KEY (id) REFERENCES Media(id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS Lists (
+						id INTEGER PRIMARY KEY,
+						profile_id INTEGER NOT NULL,
+						name TEXT NOT NULL,
+						filepath TEXT NOT NULL,
+						description TEXT,
+
+						UNIQUE (profile_id, id),
+
+						FOREIGN KEY (profile_id) REFERENCES Profiles(id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
+
+		self.connection
+			.execute(
+				"
+					CREATE TABLE IF NOT EXISTS List_Media (
+						profile_id INTEGER NOT NULL,
+						list_id INTEGER NOT NULL,
+						media_id INTEGER NOT NULL,
+
+						FOREIGN KEY (profile_id) REFERENCES Profiles(id),
+						FOREIGN KEY (profile_id, list_id) REFERENCES Lists(profile_id, id),
+						FOREIGN KEY (profile_id, media_ID) REFERENCES Media(profile_id, id),
+  
+						PRIMARY KEY (profile_id, list_id, media_id)
+					);
+				",
+				()
+			)
+			.map_err(|_| Errors::SqlInitializationError)?;
 
 		Ok(())
 	}
--- src-tauri/src/errors.rs
+++ src-tauri/src/errors.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub enum Errors {
 	SqlConnectionFailure,
+	SqlInitializationError,
 	SqlExecutionError,
 	DirectoryCreationError
 }
