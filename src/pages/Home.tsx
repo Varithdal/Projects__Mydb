4916d641dacf605b605f6fd6f1081680164497ac
Varithdal <rbareo25@gmail.com>
2026-09-03 23:01:01 -0400

    Just commiting this frontend framework code too(also not finished; just an outline)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,34 @@
 export default function Home() {
 	return (
 		<main>
+			{ /* New Entry */ }
+			<form>
+				<label for="name">Name: </label>
+				<input name="name" id="name" required />
+				<label for="file-path"> File Path: </label>
+				<input name="file-path" id="file-path" required />
+				<br />
+
+				<p>Media Type</p>
+				<input type="radio" value="videos" name="media-type" id="media-type-videos" required />
+				<label for="media-type-videos">Videos</label>
+				<br />
+
+				<p>File Type</p>
+				<input type="radio" name="file-type" value="file-type-mp4" required />
+				<label for="file-type-mp4">MP4</label>
+				<br />
+				<br />
+
+				<button type="submit">Submit</button>
+			</form>
+
+			<hr />
+
+			{ /* Retrive Entries */ }
+			<form>
+			</form>
+
 		</main>
 	);
 }
